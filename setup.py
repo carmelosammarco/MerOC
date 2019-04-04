3697a31f0c16c4986b5086d849293707d9872f95
Carmelo <carmelosammarco@users.noreply.github.com>
2019-04-04 14:30:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setup(name='MerOC',
-      version='0.4.1',
+      version='0.4.2',
       description='Software to download/manipulate netCDF files',
       long_description=long_description,
       long_description_content_type='text/markdown',
